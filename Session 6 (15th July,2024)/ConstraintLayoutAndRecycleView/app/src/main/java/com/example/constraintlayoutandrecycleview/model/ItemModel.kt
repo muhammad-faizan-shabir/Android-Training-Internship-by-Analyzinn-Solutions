e8015203c88d14e3fec46acc056d6639c7445a9e
faizanshabir123 <faizanshabir69@gmail.com>
2024-07-17 16:48:28 +0500

    Create ItemModel.kt

---
+++ Session 6 (15th July,2024)/ConstraintLayoutAndRecycleView/app/src/main/java/com/example/constraintlayoutandrecycleview/model/ItemModel.kt
@@ -0,0 +1,10 @@
+package com.example.constraintlayoutandrecycleview.model
+
+data class ItemModel(val image: Int,val text:String)
+
+// Model classes name criteria: ....model, PersonModel, UserModel, CarModel
+// Adapter classes name criteria: .....RecyclerViewAdapter, CarRecyclerViewAdapter, ItemRecyclerViewAdapter
+
+
+// variable name criteria: always start with small character: noOfUsers
+// name of the class is same as the name of this file
