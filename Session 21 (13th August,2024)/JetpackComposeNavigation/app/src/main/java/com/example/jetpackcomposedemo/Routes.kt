20ccb31c80afb298a6d9d64ec8affca7ad1aceff
faizanshabir123 <faizanshabir69@gmail.com>
2024-08-15 13:32:36 +0500

    Create Routes.kt

---
+++ Session 21 (13th August,2024)/JetpackComposeNavigation/app/src/main/java/com/example/jetpackcomposedemo/Routes.kt
@@ -0,0 +1,11 @@
+package com.example.jetpackcomposedemo
+
+object Routes
+{
+    val Home: String ="home"
+    val WelcomeScreen1: String="welcomescreen1"
+    val WelcomeScreen2: String ="welcomescreen2"
+    val WelcomeScreen3: String="welcomescreen3"
+    val WelcomeScreen4: String="welcomescreen4"
+}
+
